fb9c1bee13acf90dc6804e09987de636010b563a
ding liangxue <dinglx@uincore.net>
2018-03-12 10:06:19 +0800

    Create it!

---
+++ buildROS.sh
@@ -0,0 +1,17 @@
+#Project: SyncDrive
+#author: dinglx@uincore.net
+#Date: Mar 12 2018
+
+#Ubuntu 16.04 LTS (64bit) xenial
+#http://wiki.ros.org/kinetic/Installation/Ubuntu
+
+#http://wiki.ros.org/catkin/Tutorials/create_a_workspace
+
+source /opt/ros/kinetic/setup.bash
+
+#make a empty project to create develop enviromnet
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+
+
